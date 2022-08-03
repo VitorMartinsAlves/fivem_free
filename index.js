6b5c0d529b4bd1251e8a5dab7a7eb8e6481f02df
VitorMartinsAlves <vitormartinsalves99@gmail.com~>
2022-08-03 05:47:42 -0300

    node mod

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var child_process = require('child_process');
+// var child_process = require('child_process');
 
-child_process.exec('start.bat', function(error, stdout, stderr) {
-    console.log(stdout);
-});
+// var something = (function() {
+//     var executed = false;
+//     return function() {
+//         if (!executed) {
+//             executed = true;
+//             child_process.exec('start.bat', function (error, stdout, stderr) {
+//                 console.log(stdout);
+//             });
+        
+//         }
+//     };
+// })();
+
+// something();
